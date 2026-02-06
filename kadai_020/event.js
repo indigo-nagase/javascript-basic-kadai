5c1c2817fdf46651c59352d34a48465465f560a3
indigo-nagase <nagase@indigo.co.jp>
2026-02-06 18:07:31 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const text = document.getElementById("text");
+
+document.addEventListener("click", () => {
+    text.textContent = "ボタンがクリックされました";
+});
